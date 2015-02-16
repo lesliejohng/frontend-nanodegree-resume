0a3700410a122e56ed565176118dda53041d1d6c
Leslie Groombridge <lesliejohng@aol.com>
2015-02-16 09:03:06 +0000

    move name and title by prepend #header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,6 @@
 var formattedName = HTMLheaderName.replace("%data%", "Les Groombridge");
 var formattedRole = HTMLheaderRole.replace("%data%", "retired");
 
-$("#main").append(formattedName);
-$("#main").append(formattedRole);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
 
